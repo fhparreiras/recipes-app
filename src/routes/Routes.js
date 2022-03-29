6266294df4454f5f95f01b8c89a98c0649765269
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-03-29 13:10:57 -0300

    requisitos 5,6 e 7

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
-import DetailsDrink from '../pages/DetailsDrink';
-import DetailsFoods from '../pages/DetailsFoods';
-import Drinks from '../pages/Drinks';
-import DrinksRecipeProgress from '../pages/DrinksRecipeProgress';
-import Explore from '../pages/Explore';
-import ExploreDrinks from '../pages/ExploreDrinks';
-import ExploreDrinksIngredients from '../pages/ExploreDrinksIngredients';
-import ExploreFoods from '../pages/ExploreFoods';
-import ExploreFoodsIngredients from '../pages/ExploreFoodsIngredients';
-import ExploreFoodsNationalities from '../pages/ExploreFoodsNationalities';
-import FavoriteRecipes from '../pages/FavoriteRecipes';
-import Foods from '../pages/Foods';
-import FoodsRecipeProgress from '../pages/FoodsRecipeProgress';
-import NotFound from '../pages/NotFound';
-import Profile from '../pages/Profile';
-import RecipesDone from '../pages/RecipesDone';
+// import DetailsDrink from '../pages/DetailsDrink';
+// import DetailsFoods from '../pages/DetailsFoods';
+// import Drinks from '../pages/Drinks';
+// import DrinksRecipeProgress from '../pages/DrinksRecipeProgress';
+// import Explore from '../pages/Explore';
+// import ExploreDrinks from '../pages/ExploreDrinks';
+// import ExploreDrinksIngredients from '../pages/ExploreDrinksIngredients';
+// import ExploreFoods from '../pages/ExploreFoods';
+// import ExploreFoodsIngredients from '../pages/ExploreFoodsIngredients';
+// import ExploreFoodsNationalities from '../pages/ExploreFoodsNationalities';
+// import FavoriteRecipes from '../pages/FavoriteRecipes';
+// import Foods from '../pages/Foods';
+// import FoodsRecipeProgress from '../pages/FoodsRecipeProgress';
+// import NotFound from '../pages/NotFound';
+// import Profile from '../pages/Profile';
+// import RecipesDone from '../pages/RecipesDone';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route exact path="/foods" component={ Foods } />
+        {/* <Route exact path="/foods" component={ Foods } />
         <Route exact path="/drinks" component={ Drinks } />
         <Route exact path="/foods/:id" component={ DetailsFoods } />
         <Route exact path="/drinks/:id" component={ DetailsDrink } />
@@ -50,7 +50,7 @@ function Routes() {
         <Route exact path="/profile" component={ Profile } />
         <Route exact path="/done-recipes" component={ RecipesDone } />
         <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
-        <Route component={ NotFound } />
+        <Route component={ NotFound } /> */}
       </Switch>
     </BrowserRouter>
   );
