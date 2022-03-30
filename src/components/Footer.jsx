d3046c0fbdae4824ce3d479fed45deaf7510515d
Analu Knipp <analuknipp.dev@gmail.com>
2022-03-30 01:20:34 -0300

    Finaliza requisito 20 do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import '../css/footer.css';
 
 function Footer() {
   return (
     <div>
-      <footer data-testid="footer">
+      <footer data-testid="footer" className="footer">
 
         <input
           type="image"
           data-testid="drinks-bottom-btn"
           alt="drinks-bottom-btn"
+          src={ drinkIcon }
         />
 
         <input
           type="image"
           data-testid="explore-bottom-btn"
           alt="explore-bottom-btn"
+          src={ exploreIcon }
         />
 
         <input
           type="image"
           data-testid="food-bottom-btn"
           alt="food-bottom-btn"
+          src={ mealIcon }
         />
 
       </footer>
