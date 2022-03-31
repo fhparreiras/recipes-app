c5ca3c35867ebc6cc33d590fbb16c9fb1f409c92
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-03-31 19:02:14 -0300

    Requisitos 67 e 68 ok

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -6,6 +6,12 @@ function Explore() {
   return (
     <div>
       <Header title="Explore" renderSearchBar={ false } />
+      <button data-testid="explore-foods" type="button">
+        Explore Foods
+      </button>
+      <button data-testid="explore-drinks" type="button">
+        Explore Drinks
+      </button>
       <Footer />
     </div>
   );
