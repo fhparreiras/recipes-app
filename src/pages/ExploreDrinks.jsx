1f3e524903038f5d78f7fc1e78134d1320c3e0ee
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-03-31 20:37:00 -0300

    Requisito 74 ok

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-function ExploreDrinks() {
+function ExploreDrinks({ history }) {
+  const handleSurpriseBtn = async () => {
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+    const response = await fetch(url);
+    const result = await response.json();
+    const { idDrink } = result.drinks[0];
+    history.push(`/drinks/${idDrink}`);
+  };
+
   return (
     <div>
       <Header title="Explore Drinks" renderSearchBar={ false } />
+      <button
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/drinks/ingredients') }
+        type="button"
+      >
+        By Ingredient
+      </button>
+      <button
+        data-testid="explore-surprise"
+        onClick={ handleSurpriseBtn }
+        type="button"
+      >
+        Surprise me!
+      </button>
       <Footer />
     </div>
   );
 }
+
+ExploreDrinks.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+};
 
 export default ExploreDrinks;
