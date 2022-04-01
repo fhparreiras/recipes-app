ffd0681e5a67466fca0497a821edd4c33c4ffc74
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-04-01 11:58:53 -0300

    teste req 5 ok

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -3,13 +3,14 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
+import { EMAIL_INPUT, LOGIN_BTN, PASSWORD_INPUT } from './literals';
 
 describe('Testes da TELA DE LOGIN', () => {
   test('Todos os inputs e button da tela devem estar presentes', () => {
     renderWithRouter(<App />);
-    const inputEmail = screen.getByTestId('email-input');
-    const inputPassword = screen.getByTestId('password-input');
-    const loginBtn = screen.getByTestId('login-submit-btn');
+    const inputEmail = screen.getByTestId(EMAIL_INPUT);
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT);
+    const loginBtn = screen.getByTestId(LOGIN_BTN);
 
     expect(inputEmail).toBeDefined();
     expect(inputPassword).toBeDefined();
@@ -18,8 +19,9 @@ describe('Testes da TELA DE LOGIN', () => {
 
   test('A pessoa deve conseguir escrever seu email no input de email', () => {
     renderWithRouter(<App />);
-    const inputEmail = screen.getByTestId('email-input');
+    const inputEmail = screen.getByTestId(EMAIL_INPUT);
     const TYPED_EMAIL = 'email@email.com';
+
     userEvent.type(inputEmail, TYPED_EMAIL);
 
     expect(inputEmail).toHaveValue(TYPED_EMAIL);
@@ -27,10 +29,29 @@ describe('Testes da TELA DE LOGIN', () => {
 
   test('A pessoa deve conseguir escrever sua senha no input de senha', () => {
     renderWithRouter(<App />);
-    const inputPassword = screen.getByTestId('password-input');
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT);
     const TYPED_PASSWORD = 'xablau1234567';
+
     userEvent.type(inputPassword, TYPED_PASSWORD);
 
     expect(inputPassword).toHaveValue(TYPED_PASSWORD);
+  });
+
+  test('O formulário é válido só após a senha ter pelo menos 6 caracteres', () => {
+    renderWithRouter(<App />);
+    const inputEmail = screen.getByTestId(EMAIL_INPUT);
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT);
+    const loginBtn = screen.getByTestId(LOGIN_BTN);
+    const TYPED_EMAIL = 'emailTest@email.com';
+    let TYPED_PASSWORD = 'xablau';
+
+    userEvent.type(inputEmail, TYPED_EMAIL);
+    userEvent.type(inputPassword, TYPED_PASSWORD);
+    expect(inputPassword).toHaveValue(TYPED_PASSWORD);
+    expect(loginBtn).toBeDisabled();
+
+    TYPED_PASSWORD = 'xablau1234567';
+    userEvent.type(inputPassword, TYPED_PASSWORD);
+    expect(loginBtn).not.toBeDisabled();
   });
 });
---
+++ src/tests/literals.js
@@ -0,0 +1,5 @@
+export const EMAIL_INPUT = 'email-input';
+
+export const PASSWORD_INPUT = 'password-input';
+
+export const LOGIN_BTN = 'login-submit-btn';
