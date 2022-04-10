8fda288a5ed24ce971f750cfc974d61d9fd569ba
Analu Knipp <analuknipp.dev@gmail.com>
2022-04-09 21:03:29 -0300

    feat: css, explore, profile, explore foods, explore ingredients

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,25 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '../css/explore.css';
 
 function Explore({ history }) {
   return (
     <div>
       <Header title="Explore" renderSearchBar={ false } />
-      <button
-        data-testid="explore-foods"
-        onClick={ () => history.push('/explore/foods') }
-        type="button"
-      >
-        Explore Foods
-      </button>
-      <button
-        data-testid="explore-drinks"
-        onClick={ () => history.push('/explore/drinks') }
-        type="button"
-      >
-        Explore Drinks
-      </button>
+      <div>
+
+        <button
+          data-testid="explore-foods"
+          onClick={ () => history.push('/explore/foods') }
+          type="button"
+          className="explore"
+        >
+          Explore Foods
+        </button>
+        <button
+          data-testid="explore-drinks"
+          onClick={ () => history.push('/explore/drinks') }
+          type="button"
+          className="explore"
+        >
+          Explore Drinks
+        </button>
+      </div>
       <Footer />
     </div>
   );
